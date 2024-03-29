5e9f86c40eb938a72c158764c02a9ce9b9da3148
isaiah <isaiahpape@gmail.com>
2024-03-29 16:39:34 -0600

    spacing styling done

--- app/models/Jot.js
+++ app/models/Jot.js
@@ -14,20 +14,20 @@ export class Jot {
     }
     get GetJotsList() {
         return `
-        <h5 style="color: ${this.color}" onclick="app.JotsController.setActiveJot('${this.id}')">${this.name}</h5>
+        <h5 style="color: ${this.color}" onclick="app.JotsController.setActiveJot('${this.id}')">${this.name} <hr class="m-1" /></h5>
         `
     }
     get GetActiveJot() {
         return `
-        <div class="col-2">
-            <h4 style="color: ${this.color}">${this.name}</h4>
+        <div class="col-lg-2 col-12">
+            <h4 class="mt-2 style="color: ${this.color}">${this.name}<hr /></h4>
             <p>${this.GetCreatedTime}</p>
             <p>${this.GetUpdatedTime}</p>
             <p>word/characters goes here</p>
-         </div>
-        <div class="col-10">
+        </div>
+        <div class="col-lg-10 col-12">
         <form onsubmit="app.JotsController.updateBody()" id="jot-body">
-            <textarea name="text" id="jot-body" class="jot-body">${this.body}</textarea>
+            <textarea name="text" id="jot-body" class="jot-body mt-3">${this.body}</textarea>
             <button type="submit" class="btn border border-dark my-1"><i class="mdi mdi-content-save"></i></button>
             <button type="button" class="btn border border-dark my-1" onclick="app.JotsController.deleteJot('${this.id}')"><i class="mdi mdi-delete"></i></button>
         </div>
